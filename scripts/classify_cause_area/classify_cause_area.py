95435f1a40b9c0b1dbfddbe92160835d611824d6
nossbigg <chenggibson@gmail.com>
2018-04-15 10:27:19 +0800

    [Gibson] Impute organization cause areas based on direct keyword matching on organization descriptions

--- scripts/classify_cause_area/classify_cause_area.py
+++ scripts/classify_cause_area/classify_cause_area.py
@@ -4,6 +4,8 @@ import spacy
 DATA_PATH = '../../data'
 WEB_SCRAPE_CSV = DATA_PATH + '/output/web_scraping/web_scrape_v1.csv'
 CAUSE_AREA_CSV = DATA_PATH + '/input/cause_area/cause_area.csv'
+WEB_SCRAPE_IMPUTED_CAUSE_AREAS_CSV \
+    = DATA_PATH + '/output/classify_cause_area/web_scrape_imputed_cause_areas.csv'
 
 
 class CauseAreaClassifier:
@@ -16,45 +18,10 @@ class CauseAreaClassifier:
 
         web_scrape = self.read_web_scrape()
         web_scrape = self.add_fields_to_web_scrape(web_scrape)
-        pass
 
-    def add_fields_to_web_scrape(self, df):
-        def add_cleaned_org_description(df):
-            clean_and_stemmed = self.generate_clean_and_stemmed(df["description"].values)
+        web_scrape = self.score_based_on_cause_area(web_scrape, cause_area)
 
-            df["description_cleaned"] = \
-                pd.Series(clean_and_stemmed['descriptions']).values
-            df["description_cleaned_tuple"] = \
-                pd.Series(clean_and_stemmed['descriptions_tuple']).values
-
-        add_cleaned_org_description(df)
-        return df
-
-    def read_web_scrape(self):
-        return self.read_from_csv(WEB_SCRAPE_CSV)
-
-    def add_stemmed_cause_area(self, cause_area_dict):
-        def add_clean_and_stemmed(entry):
-            return {
-                'keywords_cleaned': entry['descriptions'],
-                'keywords_cleaned_tuple': entry['descriptions_tuple']
-            }
-
-        clean_and_stemmed = self.generate_clean_and_stemmed([
-            cause_area['keywords']
-            for key, cause_area in cause_area_dict.items()
-        ])
-
-        cleaned_iter = iter(clean_and_stemmed['descriptions'])
-        tuple_iter = iter(clean_and_stemmed['descriptions_tuple'])
-        return {
-            cause_area: {
-                **value,
-                'keywords_cleaned': next(cleaned_iter),
-                'keywords_cleaned_tuple': next(tuple_iter),
-            }
-            for cause_area, value in cause_area_dict.items()
-        }
+        web_scrape.to_csv(WEB_SCRAPE_IMPUTED_CAUSE_AREAS_CSV)
 
     def read_cause_area(self):
         list_cause_areas = self.read_from_csv(CAUSE_AREA_CSV).to_dict(orient='records')
@@ -83,6 +50,66 @@ class CauseAreaClassifier:
 
         return cause_areas_filtered
 
+    def add_stemmed_cause_area(self, cause_area_dict):
+        clean_and_stemmed = self.generate_clean_and_stemmed([
+            cause_area['keywords']
+            for key, cause_area in cause_area_dict.items()
+        ])
+
+        cleaned_iter = iter(clean_and_stemmed['descriptions'])
+        set_iter = iter(clean_and_stemmed['descriptions_set'])
+        return {
+            cause_area: {
+                **value,
+                'keywords_cleaned': next(cleaned_iter),
+                'keywords_cleaned_set': next(set_iter),
+            }
+            for cause_area, value in cause_area_dict.items()
+        }
+
+    def read_web_scrape(self):
+        return self.read_from_csv(WEB_SCRAPE_CSV)
+
+    def add_fields_to_web_scrape(self, df):
+        def add_cleaned_org_description():
+            clean_and_stemmed = self.generate_clean_and_stemmed(df["description"].fillna("").values)
+
+            df["description_cleaned"] = \
+                pd.Series(clean_and_stemmed['descriptions']).values
+            df["description_cleaned_set"] = \
+                pd.Series(clean_and_stemmed['descriptions_set']).values
+
+        add_cleaned_org_description()
+        return df
+
+    @staticmethod
+    def score_based_on_cause_area(df, cause_area):
+        cause_area_names = list(cause_area.keys())
+
+        def add_cause_area_columns():
+            for cause_name in cause_area_names:
+                df['cause_area_' + cause_name] = 0
+
+        add_cause_area_columns()
+
+        def score_organizations_by_cause_area(organization_descriptions, cause_area_keywords_set):
+            counters = [
+                len(set.intersection(description_set, cause_area_keywords_set))
+                for description_set in organization_descriptions
+            ]
+
+            return pd.Series(counters).values
+
+        def do_scoring():
+            for cause_area_name, value in cause_area.items():
+                keywords_set = value['keywords_cleaned_set']
+                df['cause_area_' + cause_area_name] = score_organizations_by_cause_area(
+                    df['description_cleaned_set'], keywords_set)
+
+        do_scoring()
+
+        return df
+
     @staticmethod
     def generate_clean_and_stemmed(list_docs):
         nlp = spacy.load('en', disable=['parser', 'tagger', 'ner'])
@@ -95,14 +122,14 @@ class CauseAreaClassifier:
                 if not word.is_stop and not word.is_punct
             ]
 
-            lemmatized_keywords.append(tuple(important_words))
+            lemmatized_keywords.append(set(important_words))
 
             sentence = " ".join(important_words)
             lemmatized_descriptions.append(sentence)
 
         return {
             'descriptions': lemmatized_descriptions,
-            'descriptions_tuple': lemmatized_keywords
+            'descriptions_set': lemmatized_keywords
         }
 
     @staticmethod
