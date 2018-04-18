bb05746229d0f68a6d6d16a01cfbbf50b259120a
Joshua Hendinata <jhendinata3@gatech.edu>
2018-04-18 22:00:15 +0800

    Removed v2_aft_google and added v3_aft_google (consolidated) Modified google_search_scrapper.py to handle exception

--- scripts/web_scraping/google_search_scrapper.py
+++ scripts/web_scraping/google_search_scrapper.py
@@ -4,9 +4,10 @@ import requests
 import math
 import time
 import random
+import traceback
 
-source_csv = 'web_scrape_v2.csv'
-target_csv = 'web_scrape_v2_aft_google.csv'
+source_csv = 'web_scrape_v3_aft_google'
+target_csv = 'web_scrape_v3_aft_google1.csv'
 
 
 # source_csv = 'wikiwand_thailand.csv'
@@ -33,58 +34,71 @@ lat_long_counter = 0
 for i in range(result_file.shape[0]):
     # if not isinstance(result_file.ix[i]['address'], str) and not isinstance(result_file.ix[i]['contact_number'], str):
     #     continue
-
     ngo_name = result_file['name'][i]
+
+    if not isinstance(ngo_name, str):
+        continue
 
     print('processing:' + str(i) + '\t' + ngo_name)
     additional_search_words = ['', 'NGO']
 
-    # time.sleep(random.random() * 3)
+    time.sleep(random.random() * 3)
 
     found = False
     for each_add_search_word in additional_search_words:
 
-        search_result_page = requests.get('https://www.google.com/search?q=' + ngo_name.replace(' ', '%20') + '%20' + each_add_search_word)
+        error_ind = True
+        while error_ind:
+            try:
+                search_result_page = requests.get('https://www.google.com/search?q=' + ngo_name.replace(' ', '%20') + '%20' + each_add_search_word)
 
-        # in case google catch you, wait for 10 mins
-        if 'CAPTCHA' in str(search_result_page.content):
-            print('caught by google. sleep for 10 mins')
-            time.sleep(600)
-            search_result_page = requests.get('https://www.google.com/search?q=' + ngo_name.replace(' ', '%20') + '%20' + each_add_search_word)
+                # in case google catch you, wait for 10 mins
+                if 'CAPTCHA' in str(search_result_page.content):
+                    print('caught by google. sleep for 10 mins')
+                    time.sleep(600)
+                    search_result_page = requests.get('https://www.google.com/search?q=' + ngo_name.replace(' ', '%20') + '%20' + each_add_search_word)
 
-        search_result_soup = BeautifulSoup(search_result_page.content, 'html.parser')
+                search_result_soup = BeautifulSoup(search_result_page.content, 'html.parser')
 
-        # search address
-        a_list = search_result_soup.find_all('a')
-        for each_a in a_list:
-            if 'LvEtkb' in each_a.get_attribute_list('class'):
-                addr_href = each_a.get('href')
-                address_str = addr_href[addr_href.find('addr') + 5:addr_href.find('&ved')].replace('+', ' ')
-                result_file.set_value(i, 'address_google', address_str)
-                found = True
-                address_counter+=1
+                # search address
+                a_list = search_result_soup.find_all('a')
+                for each_a in a_list:
+                    if 'LvEtkb' in each_a.get_attribute_list('class'):
+                        addr_href = each_a.get('href')
+                        address_str = addr_href[addr_href.find('addr') + 5:addr_href.find('&ved')].replace('+', ' ')
+                        result_file.set_value(i, 'address_google', address_str)
+                        found = True
+                        address_counter+=1
 
-        # search contact
-        span_list = search_result_soup.find_all('span')
-        for each_span in span_list:
-            if 'A1t5ne' in each_span.get_attribute_list('class'):
-                if each_span.text.replace(' ', '').replace('+', '').isdigit():
-                    result_file.set_value(i, 'contact_number_google', each_span.text)
-                    tel_counter+=1
-                    found = True
+                # search contact
+                span_list = search_result_soup.find_all('span')
+                for each_span in span_list:
+                    if 'A1t5ne' in each_span.get_attribute_list('class'):
+                        if 'Phone' in each_span.parent.span.text:
+                            result_file.set_value(i, 'contact_number_google', each_span.text)
+                            tel_counter+=1
+                            found = True
 
-        # search lat long
-        div_list = search_result_soup.find_all('div')
-        for each_div in div_list:
-            if 'R8KuR' in each_div.get_attribute_list('class'):
-                maps_href = each_div.a.get('href')
-                if maps_href.find('ll=') == -1:
-                    continue
-                lat_long_list = maps_href[maps_href.find('ll=')+3:maps_href.find('&z')].split(',')
-                result_file.set_value(i, 'lat', lat_long_list[0])
-                result_file.set_value(i, 'long', lat_long_list[1])
-                lat_long_counter+=1
-                found = True
+                # search lat long
+                div_list = search_result_soup.find_all('div')
+                for each_div in div_list:
+                    if 'R8KuR' in each_div.get_attribute_list('class'):
+                        maps_href = each_div.a.get('href')
+                        if maps_href.find('ll=') == -1:
+                            continue
+                        lat_long_list = maps_href[maps_href.find('ll=')+3:maps_href.find('&z')].split(',')
+                        result_file.set_value(i, 'lat', lat_long_list[0])
+                        result_file.set_value(i, 'long', lat_long_list[1])
+                        lat_long_counter+=1
+                        found = True
+
+                error_ind = False
+            except Exception as e:
+                error_ind = True
+                #print(traceback.format_exc())
+                traceback.print_exc()
+                print('Exception caught! Sleeping for 15 mins')
+                time.sleep(900)
 
         if found:
             print('found!')
