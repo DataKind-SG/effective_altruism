11fcf3751c769f4ed3d4153dbbeea7e7474f2b85
nossbigg <chenggibson@gmail.com>
2018-05-01 15:42:10 +0800

    [Gibson] Save preprocessed web scrape and cause area data to pickle

--- scripts/classify_cause_area/preprocess.py
+++ scripts/classify_cause_area/preprocess.py
@@ -1,14 +1,14 @@
 from scripts.classify_cause_area.helpers \
     import get_cleaned_descriptions, get_sentence_from_list, \
-    get_word_counts, get_distinct_words, read_from_csv
+    get_word_counts, get_distinct_words, read_from_csv, remove_rows_with_null_or_empty
 
 DATA_PATH = '../../data'
 
 WEB_SCRAPE_CSV = DATA_PATH + '/output/web_scraping/web_scrape_v1_trunc.csv'
 CAUSE_AREA_CSV = DATA_PATH + '/input/cause_area/cause_area.csv'
 
-WEB_SCRAPE_PREPROCESSED_CSV = DATA_PATH + '/output/classify_cause_area/web_scrape_preprocessed.csv'
-CAUSE_AREA_PREPROCESSED_CSV = DATA_PATH + '/output/classify_cause_area/cause_area_preprocessed.csv'
+WEB_SCRAPE_PREPROCESSED = DATA_PATH + '/output/classify_cause_area/web_scrape_preprocessed'
+CAUSE_AREA_PREPROCESSED = DATA_PATH + '/output/classify_cause_area/cause_area_preprocessed'
 
 WEB_SCRAPE_DESCRIPTION_COLUMN_NAME = 'description'
 CAUSE_AREA_KEYWORDS_COLUMN_NAME = 'Keywords'
@@ -22,15 +22,17 @@ class Preprocess:
         web_scrape = self.add_cleaned_text_field(web_scrape, WEB_SCRAPE_DESCRIPTION_COLUMN_NAME,
                                                  'description')
 
-        web_scrape.to_csv(WEB_SCRAPE_PREPROCESSED_CSV, encoding='ISO-8859-1', index=False)
-
         cause_area = read_from_csv(CAUSE_AREA_CSV)
         cause_area[CAUSE_AREA_KEYWORDS_COLUMN_NAME] \
             = cause_area[CAUSE_AREA_KEYWORDS_COLUMN_NAME].fillna('')
         cause_area = self.add_cleaned_text_field(cause_area, CAUSE_AREA_KEYWORDS_COLUMN_NAME,
                                                  'keywords')
 
-        cause_area.to_csv(CAUSE_AREA_PREPROCESSED_CSV, encoding='ISO-8859-1', index=False)
+        web_scrape.to_csv(WEB_SCRAPE_PREPROCESSED + ".csv", encoding='ISO-8859-1', index=False)
+        web_scrape.to_pickle(WEB_SCRAPE_PREPROCESSED + ".pl")
+
+        cause_area.to_csv(CAUSE_AREA_PREPROCESSED + ".csv", encoding='ISO-8859-1', index=False)
+        cause_area.to_pickle(CAUSE_AREA_PREPROCESSED + ".pl")
 
     @staticmethod
     def add_cleaned_text_field(df, data_column, new_column_prefix):
