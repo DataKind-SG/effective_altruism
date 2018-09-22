1efa32e475ddc20041b1d60a719fc2d0ffb01492
nossbigg <chenggibson@gmail.com>
2018-09-22 17:12:31 +0800

    [Gibson] Optimize classification of cause areas and export data for viz

--- eda/classify_cause_area/filepaths.py
+++ eda/classify_cause_area/filepaths.py
@@ -1,7 +1,10 @@
 ROOT_PATH = '../..'
 
-WEB_SCRAPE_CSV = ROOT_PATH + '/data/output/web_scraping/web_scrape_geocoded.csv'
+OUTPUT_PATH = ROOT_PATH + '/data/output'
+INPUT_PATH = ROOT_PATH + '/data/input'
+
+WEB_SCRAPE_CSV = OUTPUT_PATH + '/web_scraping/web_scrape_geocoded.csv'
 DESCRIPTION_COLUMN = 'description'
 
-EA_CSV = ROOT_PATH + '/data/input/classification_keywords/classification_keywords.csv'
+EA_CSV = INPUT_PATH + '/classification_keywords/classification_keywords.csv'
 KEYWORDS_COLUMN = 'Keywords_Set 1'
